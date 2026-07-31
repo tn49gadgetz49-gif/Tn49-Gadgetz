1d405d393510654fafbf785f4cfa3c42e22f0785
tn49gadgetz49-gif <tn49gadgetz49@gmail.com>
2026-07-31 15:17:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+import { db } from "./firebase.js";
+import {
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
 
+window.testFirebase = async function () {
+  try {
+    await addDoc(collection(db, "customers"), {
+      name: "Test Customer",
+      mobile: "9999999999",
+      prize: "Testing",
+      createdAt: new Date()
+    });
+
+    alert("✅ Firestore Connected Successfully!");
+  } catch (e) {
+    console.error(e);
+    alert("❌ Error: " + e.message);
+  }
+};
