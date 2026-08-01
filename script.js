eb2c90dc1061af31ab72f52f91bceaf811a09e73
tn49gadgetz49-gif <tn49gadgetz49@gmail.com>
2026-08-01 12:38:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+// Import Firebase
+import { db } from "./firebase.js";
 
+import {
+  collection,
+  addDoc,
+  query,
+  where,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+// HTML Elements
+const wheel = document.getElementById("wheel");
+const spinBtn = document.getElementById("spinBtn");
+const result = document.getElementById("result");
+
+const nameInput = document.getElementById("customerName");
+const mobileInput = document.getElementById("customerMobile");
+
+// Prize List
+const prizes = [
+  "🎧 Bluetooth Neckband",
+  "🔌 USB Charging Cable",
+  "🛡️ Tempered Glass",
+  "📱 Mobile Stand",
+  "🎧 Wireless Earbuds",
+  "😔 Better Luck Next Time"
+];
+
+// Variables
+let spinning = false;
